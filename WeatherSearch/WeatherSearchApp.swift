eb81d1a0d10a6a429d9e28e33ae3a6c9fcba6efc
Weixi779 <1349750521@qq.com>
2021-03-08 19:31:38 +0800

    Initial Commit

---
+++ WeatherSearch/WeatherSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSearchApp.swift
+//  WeatherSearch
+//
+//  Created by 孙世伟 on 2021/3/8.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
